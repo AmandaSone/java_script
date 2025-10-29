f6885c1030f3597c102471c6464a939cca995424
Your Name <your.email@example.com>
2025-10-29 15:24:58 +0100

    java script

--- testing.js
+++ testing.js
@@ -1,8 +1,45 @@
-console.log('Hello, World!');
+/* console.log('eyup');
 
-let antall = 5;
-let ganger = 10;
-let sum = (antall * ganger);
+let alder = 16;
+console.log('Alder er:', alder, 'år gammel');
+alder = alder + 1;
 
-console.log(antall);
-console.log(sum);
+let erStudent = true;
+let erVoksen = false;
+
+let a = 6;
+let b = 8;
+let sum = a + b;
+console.log('Summen av a og b er:', sum);
+ */
+
+//---------------------------------------------
+
+
+/* let antall = 5;
+console.log('Antall er:', antall);
+antall = antall + 2;
+console.log('Nytt antall er:', antall); */
+
+
+//---------------------------------------------
+
+//nsole.log('Hei, jeg lærer meg java script');
+let navn = 'Amanda';
+console.log('Mitt navn er:', navn);
+
+const dato = 7;
+const måned = 'September';
+const år = 2009;
+
+let alder2 = 16;
+console.log('Jeg er', alder2, 'år gammel');
+alder2 = alder2 + 1;
+console.log('Neste år blir jeg', alder2, 'år gammel');
+
+//---------------------------------------------
+
+const hilsen = `Hei ${navn}`;
+console.log(hilsen);
+const fødselsdato = `${dato}. ${måned} ${år}`;
+console.log('Min fødselsdato er:', fødselsdato);
